3bb15f7e270b0d6da2787aadd656b03a696b845b
Bobby Lee <superbahbi@users.noreply.github.com>
2022-06-23 20:15:53 -0700

    added new assets

--- src/Assets/Images.js
+++ src/Assets/Images.js
@@ -6,4 +6,35 @@ import Zomato from "./zomato.png";
 import Services1 from "./services-1.png";
 import Services2 from "./services-2.png";
 import Services3 from "./services-3.png";
-export { Gojek, Grab, Dana, Shopee, Zomato, Services1, Services2, Services3 };
+import Trending1 from "./trending-1.jpg";
+import Trending2 from "./trending-2.jpg";
+import Trending3 from "./trending-3.jpg";
+import Trending4 from "./trending-4.jpg";
+import Trending5 from "./trending-5.jpg";
+import Trending6 from "./trending-6.jpg";
+import Trending7 from "./trending-7.jpg";
+import ReviewerAvatar from "./reviewer-avatar.svg";
+import ReviewImage from "./review-image.png";
+import NewsletterImage from "./newsletter-image.png";
+import Star from "./star.svg";
+export {
+  Gojek,
+  Grab,
+  Dana,
+  Shopee,
+  Zomato,
+  Services1,
+  Services2,
+  Services3,
+  Trending1,
+  Trending2,
+  Trending3,
+  Trending4,
+  Trending5,
+  Trending6,
+  Trending7,
+  ReviewerAvatar,
+  ReviewImage,
+  NewsletterImage,
+  Star,
+};
