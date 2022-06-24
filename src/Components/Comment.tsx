c06313bb9ea51e53caf35cc704593e8e398de07c
Bobby Lee <superbahbi@users.noreply.github.com>
2022-06-23 20:16:13 -0700

    added comment component

---
+++ src/Components/Comment.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+const CommentContainer = styled.div``;
+
+interface CommentProps {}
+
+const Comment: React.FC<CommentProps> = ({}) => {
+  return <CommentContainer></CommentContainer>;
+};
+export default Comment;
